b083e95e1c7a537bc838a7c52cff0a32f6cf941b
MichaelNkong <nkongho.ndip@yahoo.com>
2020-08-31 09:04:01 +0200

    first commit

---
+++ src/Controller/ToDoListController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType; 
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType; 
+use Symfony\Component\Form\Extension\Core\Type\FormType; 
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use App\Entity\Tasks;
+use Symfony\Component\Security\Core\User\UserInterface;
+class ToDoListController extends AbstractController
+{
+    /**
+     * @Route("/", name="create_new")
+
+     */
+    public function index()
+    {
+       $articles = $this->getDoctrine()->getRepository(Tasks::class)->findAll();
+       return $this->render('dodolist/index.html.twig', array('articles'=>$articles));
+    }
+
+    
+
+/**
+     * @Route("/article/new", name="new_article")
+    
+    
+
+     */
+  public function  new(Request $request){
+
+  $task = new Tasks();
+  $form = $this->createFormBuilder($task)
+
+  ->add('title', TextType::class,array('attr'=> array('class'=>'form-control ')))
+  ->add('status', TextType::class,array('attr'=> array( 'class'=>'form-control ')))
+  ->add('save', SubmitType::class,array('label'=> 'Create' , 'attr'=>array('class'=>'btn btn-primary mt-3')))
+  
+   ->getForm();
+   $form->handleRequest($request);
+   if($form->isSubmitted() && $form->isValid())
+   {
+     $task= $form->getData();
+     $entityManager = $this->getDoctrine()->getManager();
+     $entityManager->persist($task);
+     $entityManager->flush();
+     return $this->redirectTORoute('create_new');
+   }
+   return $this->render('dodolist/new.html.twig' , array('form'=>$form->createView()));
+}
+/**
+     * @Route("/article/edit/{id}", name="edit_article")
+    
+    
+
+     */
+    public function  edit(Request $request, $id){
+
+        
+        $tasks=$this->getDoctrine()->getRepository(Tasks::class)->find($id);
+        $form = $this->createFormBuilder($tasks)
+      
+        ->add('title', TextType::class,array('attr'=> array('class'=>'form-control ')))
+        ->add('status', TextType::class,array('attr'=> array( 'class'=>'form-control ')))
+        ->add('save', SubmitType::class,array('label'=> 'Create' , 'attr'=>array('class'=>'btn btn-primary mt-3')))
+        
+         ->getForm();
+         $form->handleRequest($request);
+         if($form->isSubmitted() && $form->isValid())
+         {
+           
+           $entityManager = $this->getDoctrine()->getManager();
+           $entityManager->flush();
+           return $this->redirectTORoute('create_new');
+         }
+         return $this->render('dodolist/edit.html.twig' , array('form'=>$form->createView()));
+      }
+/**
+     * @Route("/article/{id}", name="show_article")
+    
+
+     */
+    public function show($id){
+
+        $article=$this->getDoctrine()->getRepository(Tasks::class)->find($id);
+        
+        return $this->render("dodolist/show.html.twig",array('article'=>$article));
+        
+        }
+        /**
+          * @Route("/article/delete/{id}", name="delete_article")
+
+         */
+    public function delete(Request $request , $id){
+
+        $tasks=$this->getDoctrine()->getRepository(Tasks::class)->find($id);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($tasks);
+        $entityManager->flush();
+        
+        
+      $response = new Response();
+        $response->send();
+        }
+        
+}
---
+++ src/Entity/Tasks.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use App\Repository\TasksRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TasksRepository::class)
+ */
+class Tasks
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+    public function __toString()
+  {
+      return $this->getStatus();
+    
+  }
+  
+
+
+}
