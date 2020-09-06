668834253d926074f8c55721218a7e93d03148d5
MichaelNkong <nkongho.ndip@yahoo.com>
2020-09-06 10:54:09 +0200

    added search feature

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType; 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType; 
 use Symfony\Component\Form\Extension\Core\Type\FormType; 
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -54,6 +55,44 @@ class ToDoListController extends AbstractController
    }
    return $this->render('dodolist/new.html.twig' , array('form'=>$form->createView()));
 }
+
+     /**
+     * @Route("/article/form", name="form _article")
+    
+
+     */
+    public function  form(Request $request){
+
+      
+      $form = $this->createFormBuilder()
+
+      
+      ->add('article', TextType::class,array( 'attr'=> array('class'=>'form-control ' )))
+      ->add('search', SubmitType::class,array('label'=> 'search' , 'attr'=>array('class'=>'btn btn-primary mt-3')))
+      
+       ->getForm();
+      $form->handleRequest($request);
+   if($form->isSubmitted() && $form->isValid())
+   {
+    
+    $data = $request->request->get('form')['article'];
+    
+    $em = $this->getDoctrine()->getManager();
+    $query = $em->createQuery(
+    'SELECT p.title FROM App:Tasks p
+      WHERE p.title LIKE :data')
+      ->setParameter('data', $data);
+      $res = $query->getResult();
+      
+      return $this->render('dodolist/resultsearch.html.twig', array('articles'=>$res));
+    
+   }
+      
+       return $this->render('dodolist/search.html.twig' , array('form'=>$form->createView()));
+    }
+    
+
+     
 /**
      * @Route("/article/edit/{id}", name="edit_article")
     
@@ -66,7 +105,7 @@ class ToDoListController extends AbstractController
         $tasks=$this->getDoctrine()->getRepository(Tasks::class)->find($id);
         $form = $this->createFormBuilder($tasks)
       
-        ->add('title', TextType::class,array('attr'=> array('class'=>'form-control ')))
+        ->add('title', TextType::class, array('attr'=> array('class'=>'form-control ')))
         ->add('status', TextType::class,array('attr'=> array( 'class'=>'form-control ')))
         ->add('save', SubmitType::class,array('label'=> 'Create' , 'attr'=>array('class'=>'btn btn-primary mt-3')))
         
@@ -93,6 +132,7 @@ class ToDoListController extends AbstractController
         return $this->render("dodolist/show.html.twig",array('article'=>$article));
         
         }
+       
         /**
           * @Route("/article/delete/{id}", name="delete_article")
 
@@ -103,9 +143,7 @@ class ToDoListController extends AbstractController
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->remove($tasks);
         $entityManager->flush();
-        
-        
-      $response = new Response();
+        $response = new Response();
         $response->send();
         }
         
